7e677c05795e7d55cfe2371632a12786e40ab56b
Adam Jachowicz <jachowiczus@gmail.com>
2015-05-03 13:15:39 +0200

    First change, added LDisplay basic content

--- LChar/src/tk/adamj57/LChar/LDisplay.java
+++ LChar/src/tk/adamj57/LChar/LDisplay.java
@@ -1,5 +1,33 @@
 package tk.adamj57.LChar;
 
+import com.rngtng.launchpad.LColor;
+import com.rngtng.launchpad.Launchpad;
+
 public class LDisplay {
 
+	private Launchpad lp;
+	
+	public LDisplay(Launchpad lp)
+	{
+		this.setLp(lp);
+	}
+
+	public Launchpad getLp() {
+		return lp;
+	}
+
+	public void setLp(Launchpad lp) {
+		this.lp = lp;
+	}
+	
+	public void display(LChar character){
+		
+		display(character, LColor.HIGH);
+	}
+	
+	public void display(LChar character, int color){
+		
+		//TODO wyœwietlanie
+	}
+	
 }
