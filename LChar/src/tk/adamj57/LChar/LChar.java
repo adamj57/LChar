3ddaf699f6ed50f0089778c0f5cf83d2c26deea4
Adam Jachowicz <jachowiczus@gmail.com>
2015-05-04 18:03:34 +0200

    Added UndefinedCharException Added basic conversion methods in LChar Added getter for pixelList

--- LChar/src/tk/adamj57/LChar/LChar.java
+++ LChar/src/tk/adamj57/LChar/LChar.java
@@ -1,5 +1,50 @@
 package tk.adamj57.LChar;
 
+import java.awt.Point;
+
 public enum LChar {
 
+	a(new Point[]{}, 'a'),
+	b(new Point[]{}, 'b');
+	
+	private Point[] pixelList;
+	private char character;
+	
+	private LChar(Point[] pixelList, char character){
+		
+		this.pixelList = pixelList;
+		this.character = character;
+		
+	}
+	
+	public Point[] getPixelList(){
+		
+		return pixelList;
+	}
+	
+	public static char toChar(LChar lChar){
+		
+		return lChar.character;
+	}
+	
+	public static LChar toLChar(char character) throws UndefinedCharException{
+		
+		LChar[] all = LChar.values();
+		
+		for(LChar lChar : all){
+			
+			if(character == LChar.toChar(lChar)){
+				
+				return lChar;
+			}
+			
+		}
+		
+		throw new UndefinedCharException();
+	}
+	
+	private Point createPoint(int x, int y){
+		
+		return new Point(x, y);
+	}
 }
---
+++ LChar/src/tk/adamj57/LChar/UndefinedCharException.java
@@ -0,0 +1,6 @@
+package tk.adamj57.LChar;
+
+@SuppressWarnings("serial")
+public class UndefinedCharException extends Exception {
+
+}
