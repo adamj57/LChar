25eeb80c0143828004e3c4a20a97b75a234def29
Adam Jachowicz <jachowiczus@gmail.com>
2015-05-28 23:36:48 +0200

    Deleted Scrolling feature, it's too hard to program, maybe sometime ;)

--- LChar/src/tk/adamj57/LChar/LDisplay.java
+++ LChar/src/tk/adamj57/LChar/LDisplay.java
@@ -2,7 +2,6 @@ package tk.adamj57.LChar;
 
 import java.awt.Point;
 import java.util.ArrayList;
-
 import com.rngtng.launchpad.LColor;
 import com.rngtng.launchpad.Launchpad;
 
@@ -48,103 +47,7 @@ public class LDisplay {
 		}
 	}
 	
-	public void wordDisplay(LChar[] word){
-		
-		ArrayList<Point> panoramaToDisplay = toPanorama(word);
-		
-		scroll(panoramaToDisplay);
-		
-		
-	}
-	
-	private void scroll(ArrayList<Point> panoramaToDisplay){
-		
-		boolean hasPointsToDisplay = true;
-		ArrayList<Point> pointsToDisplay = new ArrayList<Point>();
-		
-		while(hasPointsToDisplay){
-			
-			for(Point point : panoramaToDisplay){
-				
-				if(point.getX() <= 7  && point.getX() >=0){
-					
-					pointsToDisplay.add(point);
-					
-				}
-			}
-			
-			
-			
-			display((Point[]) pointsToDisplay.toArray());
-			
-			pointsToDisplay.clear();
-			
-			for(int i = 0; i < panoramaToDisplay.size(); i++){
-				
-				panoramaToDisplay.get(i).setLocation(panoramaToDisplay.get(i).getX() - 1, panoramaToDisplay.get(i).getY());
-				
-			}
-			hasPointsToDisplay = false;
-			for(int i = 0; i < panoramaToDisplay.size(); i++){
-				
-				if(panoramaToDisplay.get(i).getX() > -1){
-					
-					hasPointsToDisplay = true;
-				}
-				
-			}
-			
-			
-			
-		}
-		
-		
-		
-		
-	}
-
-	private ArrayList<Point> toPanorama(LChar[] word) {
-		
-		ArrayList<Point> pointWord = new ArrayList<Point>();
-		
-		//ustalanie koordynat mozajki
-		for (int i = 0; i < word.length; i++){
-			
-			//je¿eli spacja, pomijamy i dodajemy przerwê 8 pikseli
-			if (word[i] == LChar.space){
-				
-				continue;
-				
-			}
-			//zbiór pixeli jednej litery
-			Point[] pixelList = word[i].getPixelList();
-			
-			//ustawiamy koordynaty do mozajki
-			for(int j = 0; j < pixelList.length; j++){
-				
-				pixelList[j].setLocation(pixelList[j].getX() + (8 * (i + 1)), pixelList[j].getY());
-				
-			}
-			
-			//dodajemy gotowy znak w postaci punktów do pointWord
-			for(Point pixel : pixelList){
-				
-				pointWord.add(pixel);
-				
-			}
-			
-			
-		}
-		return pointWord;
-	}
 	
 	
-	public void wordDisplay(LWord word){
-		
-		LChar[] LCharWord = word.toArray();
-		
-		wordDisplay(LCharWord);
-		
-	}
 	
 }
--- LChar/src/tk/adamj57/LChar/Testing.java
+++ LChar/src/tk/adamj57/LChar/Testing.java
@@ -12,7 +12,7 @@ public class Testing {
 
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws UndefinedCharException{
 		
 		LDisplay lp = new LDisplay(new Launchpad(new PApplet(), "Launchpad Mini", "Launchpad Mini"));
 		
@@ -30,35 +30,15 @@ public class Testing {
 				}
 			}
 			
+
 		
 		
-		//TODO program daj¹cy mo¿liwoœæ narysowania litery na lp i przekonwertowaniu jej do postaci poleceñ
+			lp.getLp().dispose();
 		
-		LChar[] name = new LChar[]{
-				LChar.A, LChar.n, LChar.t, LChar.e, LChar.k
-				
-		};
-		
-		
-		
-		for(LChar letter : name){
-			
-			lp.display(letter);
-			
-			String quit = new Scanner(System.in).nextLine();
-			/*
-			try {
-				Thread.sleep(2000L);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
-			lp.display(allEmpty, LColor.GREEN_OFF);
-		}
+	
 	
 		
-		lp.getLp().dispose();
+		
 		
 	}
 }
